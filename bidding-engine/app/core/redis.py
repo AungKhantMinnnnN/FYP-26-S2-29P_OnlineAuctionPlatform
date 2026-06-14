9923a4bda8c999ec4d73f0b7525ca20c45b55f3a
AungKhantMinn <108041599+AungKhantMinnnnN@users.noreply.github.com>
2026-06-14 17:20:14 +0800

    Requirement to update redis to rediss

--- bidding-engine/app/core/redis.py
+++ bidding-engine/app/core/redis.py
@@ -5,8 +5,12 @@ import logging
 logger = logging.getLogger("BiddingEngine")
 
 # Initialize global Redis client for locking and caching
+url = settings.REDIS_URL
+if "upstash.io" in url and url.startswith("redis://"):
+    url = url.replace("redis://", "rediss://")
+
 redis_client = aioredis.from_url(
-    settings.REDIS_URL,
+    url,
     encoding="utf-8",
     decode_responses=True,
     max_connections=10
