dfdb4d6c44251f502b1961a382cb35231c22ad5e
AungKhantMinn <108041599+AungKhantMinnnnN@users.noreply.github.com>
2026-07-01 22:58:01 +0800

    Update to upload and get marketing video backend

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.controller import health, auth, auctions, testimonials, disputes, users, subscriptions, issue_types, boards
+from app.api.v1.controller import health, auth, auctions, testimonials, disputes, users, subscriptions, issue_types, boards, marketing
 
 api_router = APIRouter()
 
@@ -12,3 +12,4 @@ api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(subscriptions.router, prefix="/subscription-tiers", tags=["subscriptions"])
 api_router.include_router(issue_types.router, prefix="/issue-types", tags=["issue-types"])
 api_router.include_router(boards.router, prefix="/boards", tags=["boards"])
+api_router.include_router(marketing.router, tags=["marketing"])
